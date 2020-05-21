d60003ff1ac18f5a4a61f894018d8c9d0deb041c
Luis Cardozo <me@luiscardozo.dev>
2020-05-21 18:33:20 -0400

    feat: Script to start everything in one shot

---
+++ start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#My local installation, as the ffmpeg installed via apt does not have ffserver anymore
+FFSERVER=/opt/bin/ffserver/bin/ffserver
+FFMPEG=/opt/bin/ffserver/bin/ffmpeg
+
+UI_SERVER_URL=http://localhost:3000
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#prepare the logs
+LOGDIR=$SCRIPT_DIR/logs
+mkdir -p $LOGDIR
+
+# run the Mosca Server
+cd $SCRIPT_DIR/webservice/server/node-server/
+exec node server.js > $LOGDIR/mosca.log 2> $LOGDIR/mosca_err.log &
+
+# run the UI server
+cd $SCRIPT_DIR/webservice/server/ui/
+exec npm run dev > $LOGDIR/ui.log 2> $$LOGDIR/ui_err.log &
+
+# run the FFMPEG server
+cd $SCRIPT_DIR/ffmpeg
+$FFSERVER -f server.conf
+
+# run the python script
+source env/bin/activate
+
+VIDEO_SIZE=768x432
+python3 main.py | $FFMPEG -v warning -f rawvideo -pixel_format bgr24 -video_size $VIDEO_SIZE -framerate 24 -i - http://0.0.0.0:3004/fac.ffm
+
+# open the UI in a browser
+xdg-open http://localhost:3000
