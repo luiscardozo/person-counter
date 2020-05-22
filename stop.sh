67b9ac0adc9cde9543f7d7473bf4a438c9599587
Luis Cardozo <me@luiscardozo.dev>
2020-05-22 11:12:26 -0400

    fix: stop.sh: Invert the test to see if a server is running

--- stop.sh
+++ stop.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 SERVER_PID=`ps aux | grep 'node server.js' | grep -v grep | awk '{print $2}'`
-if [ -z ${SERVER_PID+x} ]; then
+if [ ! -z ${SERVER_PID} ]; then
     echo "Closing MQTT Server"
     kill -INT $SERVER_PID
 fi
 
 UI_PID=`ps aux | grep 'node.*webservice\/ui.*' | grep -v "NODE_ENV" | awk '{print $2}'`
-if [ -z ${UI_PID+x} ]; then
+if [ ! -z ${UI_PID} ]; then
     echo "Closing UI"
     kill -INT $UI_PID
 fi
 
 FF_PID=`ps aux | grep 'ffserver' | grep -v grep | awk '{print $2}'`
-if [ -z ${FF_PID+x} ]; then
+if [ ! -z ${FF_PID} ]; then
     echo "Closing FFMPEG"
     kill -INT $FF_PID
 fi
