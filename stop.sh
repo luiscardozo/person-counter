8e3be5dd9847aabe6fb58785bd2c6753d1c5c816
Luis Cardozo <me@luiscardozo.dev>
2020-05-22 11:03:52 -0400

    feat: Add stop script, to stop everything in one shot

---
+++ stop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SERVER_PID=`ps aux | grep 'node server.js' | grep -v grep | awk '{print $2}'`
+if [ -z ${SERVER_PID+x} ]; then
+    echo "Closing MQTT Server"
+    kill -INT $SERVER_PID
+fi
+
+UI_PID=`ps aux | grep 'node.*webservice\/ui.*' | grep -v "NODE_ENV" | awk '{print $2}'`
+if [ -z ${UI_PID+x} ]; then
+    echo "Closing UI"
+    kill -INT $UI_PID
+fi
+
+FF_PID=`ps aux | grep 'ffserver' | grep -v grep | awk '{print $2}'`
+if [ -z ${FF_PID+x} ]; then
+    echo "Closing FFMPEG"
+    kill -INT $FF_PID
+fi
